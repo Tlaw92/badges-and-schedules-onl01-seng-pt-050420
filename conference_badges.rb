bea0ef4ce8925a6d619a392f0eaa4973564d0130
travis Lawson <travislawclimb@gmail.com>
2020-05-17 18:57:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,7 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 
+
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(attendees)
@@ -10,9 +11,12 @@ def batch_badge_creator(attendees)
   end
 end
 
+
 def room_assigner(attendees)
 rooms = ["room 1", "room 2", "room 3", "room 4", "room 5", "room 6", "room 7"]  
   attendees.each_with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
+
+
