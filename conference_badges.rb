f4a4ec106b18ac61398299cbec34a666300395ba
travis Lawson <travislawclimb@gmail.com>
2020-05-17 18:57:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,6 @@ end
 
 
 def room_assigner(attendees)
-rooms = ["room 1", "room 2", "room 3", "room 4", "room 5", "room 6", "room 7"]  
   attendees.each_with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
