4f57defbd5f0a34dcc2303413fe41e3529509f4c
travis Lawson <travislawclimb@gmail.com>
2020-05-17 18:39:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,6 @@ end
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(attendees)
-  sentances = []
   attendees.map do |attendee|
     "Hello, my name is #{attendee}"
   end  
